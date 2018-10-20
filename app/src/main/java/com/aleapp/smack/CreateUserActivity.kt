95b03446a723811fe86a938437bc0afe4644438d
josealejandroherrerodelorenzi <jaherrero85>
2018-10-20 14:43:56 -0300

    Finish lesson3

---
+++ app/src/main/java/com/aleapp/smack/CreateUserActivity.kt
@@ -0,0 +1,25 @@
+package com.aleapp.smack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun createAvatarImageViewClicked(view:View){
+
+    }
+
+    fun createBackgroundColorBtnClicked(view: View){
+
+    }
+
+    fun createUserBtnClicked(view: View){
+
+    }
+}
